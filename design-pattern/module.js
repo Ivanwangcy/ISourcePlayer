55865454d6f09817eb37516372f57f47caa46869
Ivan <wcy5211678@163.com>
2015-07-25 11:40:56 +0800

    modified module.js file by me

--- design-pattern/module.js
+++ design-pattern/module.js
@@ -1,4 +1,4 @@
-//让我们通过创建一个自包含得模块来看一下 Module 模式
+//让我们通过创建一个自包含得模块来看一下 Module 模式的实现
 var testModule = (function () {
   var counter = 0;
   return {
@@ -16,7 +16,27 @@ var testModule = (function () {
 //
 // 增加计数器
 testModule.incrementCounter();
+testModule.incrementCounter();
+testModule.incrementCounter();
+testModule.incrementCounter();
 
 //检查计数器值并重置
-//输出: 1
+//输出: 4
 testModule.resetCounter();
+
+//下面是一个包含命名空间, 公有和私有变量的 MOdule 模式:
+
+var myNamespace = (function () {
+  var myPrivateProperty = 0;
+  var myPrivateMethod = function (name){
+    console.log("(myNamespace)" + name + " : " + myPrivateProperty);
+  }
+  return {
+    myPublicMethod: function (name) {
+      myPrivateProperty++;
+      myPrivateMethod(name);
+    }
+  }
+})();
+
+myNamespace.myPublicMethod("Hello");
