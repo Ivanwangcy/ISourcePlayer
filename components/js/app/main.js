ffc33de72d4a67ed0dbabb63eee2c07f95e18f36
Ivan <wcy5211678@163.com>
2015-07-26 16:39:09 +0800

    modified commponent file

--- components/js/app/main.js
+++ components/js/app/main.js
@@ -1,10 +1,10 @@
 // , "jquery.alpha", "jquery.beta"
-define(["jquery", "select2"], function($) {
+define(["jquery", "jquery.alpha", "jquery.beta"], function($) {
     //the jquery.alpha.js and jquery.beta.js plugins have been loaded.
-    // $(function() {
-    //     $('body').alpha().beta();
-    // });
-    $(document).ready(function() {
-      $(".js-example-basic-single").select2();
+    $(function() {
+        $('body').alpha().beta();
     });
+    // $(document).ready(function() {
+    //   $(".js-example-basic-single").select2();
+    // });
 });
