d5dd97845f19491f23c8587abb5afa6dfae9a1b7
Ivan <wcy5211678@163.com>
2015-08-11 10:11:54 +0800

    todos

--- framework/backbone/todos.js
+++ framework/backbone/todos.js
@@ -49,7 +49,17 @@ $(function () {
     initialize: function () {
       this.listenTo(this.model, 'change', this.render);
       this.listenTo(this.model, 'destroy', this.remove);
+    },
+    render: function () {
+      this.$el.html(this.template(this.model.toJSON()));
+    },
+    toggleDone: function () {
+      this.model.toggle();
+    },
+    this.remove: function () {
+
     }
+
   });
 
 
