bd7f1fd1e3fc54afd00078486f57b24b4df9c198
Ivan <wcy5211678@163.com>
2015-08-14 19:05:58 +0800

    modified

--- framework/backbone/js/todos.js
+++ framework/backbone/js/todos.js
@@ -110,7 +110,22 @@ $(function () {
       this.main = $('#main');
 
       Todos.fetch();
-    }
+    },
+    render: function() {
+      var done = Todos.done().length;
+      var remaining = Todos.remaining().length;
+
+      if(Todos.length){
+        this.main.show();
+        this.footer.show();
+        this.footer.html(this.statsTemplate({done: done, remaining: remaining}));
+      }else{
+        this.main.hide();
+        this.footer.hide();
+      }
+
+      this.allCheckbox.checked = !remaining;
+    },
     createOnEnter: function (e) {
       if(e.keyCode == 13){
 
