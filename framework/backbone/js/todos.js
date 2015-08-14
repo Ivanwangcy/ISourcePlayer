73af949f8c73d335a4ea98e3df3a427fdfe7916a
Ivan <wcy5211678@163.com>
2015-08-14 16:20:10 +0800

    modified

--- framework/backbone/js/todos.js
+++ framework/backbone/js/todos.js
@@ -1,6 +1,8 @@
 $(function () {
 
   //Todo Model
+  //--------------------
+
   var Todo = Backbone.Model.extend({
 
     // Default attribute
@@ -17,6 +19,8 @@ $(function () {
 
 
   //Todo Collection
+  //--------------------------
+
   var TodoList = Backbone.Collection.extend({
     model: Todo,
     localStorage: new Backbone.LocalStorage("todos-backbone"),
@@ -36,6 +40,8 @@ $(function () {
   var Todos = new TodoList();
 
   //Todo Item View
+  //-----------------------------
+
   var TodoView = Backbone.View.extend({
     tagName: "li",
     template: _.template($('#item-template').html()),
@@ -52,15 +58,64 @@ $(function () {
     },
     render: function () {
       this.$el.html(this.template(this.model.toJSON()));
+      this.$el.toggleClass("done", this.model.get('done'));
+      this.input = this.$('.edit');
+      return this;
     },
     toggleDone: function () {
       this.model.toggle();
     },
-    this.remove: function () {
-
+    edit: function () {
+      this.$el.addClass("editing");
+      this.input.focus();
+    },
+    close: function() {
+      var value = this.input.val();
+      if(!value){
+        this.clear();
+      }else{
+        this.model.save({title: value});
+        this.$el.removeClass("editing");
+      }
+    },
+    updateOnEnter: function (e) {
+      if(e.keyCode == 13) this.close();
+    },
+    clear: function() {
+      this.model.destroy();
     }
 
   });
 
+  // The Application
+  // -----------------------------
+
+  var AppView = Backbone.View.extend({
+    el: $("#todoapp"),
+    statsTemplate: _.template($("#stats_template").html()),
+    events: {
+      "keypress #new-todo": "createOnEnter",
+      "click #clear-completed": "clearCompleted",
+      "click #toggle-all": "toggleAllComplete"
+    },
+    initialize: function() {
+      this.input = this.$("#new-todo");
+      this.allCheckbox = this.$("toggle-all")[0];
+
+      this.listenTo(Todos, 'add', this.addOne);
+      this.listenTo(Todos, 'reset', this.addAll);
+      this.listenTo(Todos, 'all', this.render);
+
+      this.footer = this.$('footer');
+      this.main = $('#main');
+
+      Todos.fetch();
+    }
+    createOnEnter: function (e) {
+      if(e.keyCode == 13){
+
+      }
+    }
+  });
 
 });
